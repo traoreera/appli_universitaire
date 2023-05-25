a42cd09b9f3b72d66969af8951eab74668e08ef7
traoreera <traoreera@gmail.com>
2023-05-25 10:31:33 +0000

    ...

--- install.py
+++ install.py
@@ -1,12 +1,25 @@
 import typer
 import getpass
 app = typer.Typer()
+from rich import console
+from rich import print
+console = console.Console()
 
+class Login():
+    
+    def __init__(self,username,password,**arkgv):
+        self.username = username
+        self.passwd = password
+        self.arkgv = arkgv.keys()
+        
+        print()
 
 @app.command()
 def _login():
     username = input('username :')
     password = getpass.getpass('password:')
+    
+    Login(username=username,password=password)
 
 
 if __name__ == "__main__":
